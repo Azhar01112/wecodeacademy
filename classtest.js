3fe0ca542bd9622b91dbac28f55ecbd6983dd2c6
azhar01112 <ajrusss786@gmail.com>
2023-02-15 22:34:03 +0530

    add new file

---
+++ classtest.js
@@ -0,0 +1,55 @@
+// Q. 10
+
+// let myName = "azharkhatu";
+// console.log(myName.length);
+
+// Q. 1
+// let frenchfries = Number(
+//   prompt("How many french fries (60rs per piece) do you want to order ?")
+// );
+// let burgers = Number(
+//   prompt("How many burgers (50rs per piece) do you want to order ?")
+// );
+// let chowmin = Number(
+//   prompt("How many plates of chowmin (100rs per plate) do you want to order?")
+// );
+// let Manchurian = Number(
+//   prompt("How many plates of Manchurian (80rs per plate) do you want to order?")
+// );
+// let Cokes = Number(
+//   prompt("How many Cokes (50 per piece) do you want to order ?")
+// );
+// let totel =
+//   frenchfries * 60 +
+//   burgers * 50 +
+//   chowmin * 100 +
+//   Manchurian * 80 +
+//   Cokes * 50;
+// console.log(totel);
+
+//  Q.2
+
+// for (var a = 1; a <= 7; a++) {
+//   let ans = "";
+//   for (var b = 8 - a; b >= 1; b--) {
+//     ans = ans + "$##";
+//   }
+//   console.log(ans);
+// }
+
+// 4.
+
+// let num = 25;
+// switch (num) {
+//   case num % 2 === 1:
+//     console.log("odd");
+//     break;
+//   case num % 2 === 0:
+//     console.log("even");
+//     break;
+//   case num == 100:
+//     console.log(100);
+//     break;
+//   default:
+//     console.log(1000);
+// }
